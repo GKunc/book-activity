a0258a2d49fa38a3e1e30068d7203f56f0e18249
GKunc <grzeg.kunc@gmail.com>
2023-03-05 19:00:20 +0100

    Trying to make heroku work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  GOOGLE_CLIENT_ID: 'undefined',
+  GOOGLE_SECRET: 'undefined',
+  MANGO_DB_CONNECTION_STRING: 'undefined',
+  MANGO_DB_CONNECTION_STRING_PHOTOS: 'undefined',
+  API_URL: 'hhtp://localhost:8080',
+  appVersion: '0.0.0',
+  production: true,
+};
