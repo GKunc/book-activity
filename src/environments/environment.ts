0a376c48d10c6363c4317ddd429a5a41a2fd049d
GKunc <grzeg.kunc@gmail.com>
2023-03-15 17:55:36 +0100

    Empty env

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  GOOGLE_CLIENT_ID: '',
+  MANGO_DB_CONNECTION_STRING: '',
+  MANGO_DB_CONNECTION_STRING_PHOTOS: '',
+  HERE_MAPS_API_KEY: '',
+  MEASUREMENT_ID: '',
+  API_URL: 'hhtp://localhost:8080',
+  appVersion: '0.0.0',
+  production: true,
+};
