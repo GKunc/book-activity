60ff7dc8500deef60b40bbce7ad2f98d15c4e25b
GKunc <grzeg.kunc@gmail.com>
2023-04-18 17:16:30 +0200

    Fix favourites styles

--- src/app/sign/login-page/login-page.component.ts
+++ src/app/sign/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
 import { ModalService } from 'src/app/common/services/modal/modal.service';
 import { LoginService } from '../../common/services/login-service/login.service';
 import { NotificationsService } from '../../common/services/notifications/notifications.service';
@@ -8,9 +9,9 @@ import { NotificationsService } from '../../common/services/notifications/notifi
 @Component({
   selector: 'login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.less']
+  styleUrls: ['./login-page.component.less'],
 })
-export class LoginPageComponent implements OnInit { 
+export class LoginPageComponent implements OnInit {
   @Output()
   switchMode: EventEmitter<void> = new EventEmitter<void>();
 
@@ -26,8 +27,8 @@ export class LoginPageComponent implements OnInit {
     private loginService: LoginService,
     private notificationService: NotificationsService,
     private modalService: ModalService,
-    private router: Router,
-    ) {}
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.form.markAsPristine();
@@ -35,26 +36,28 @@ export class LoginPageComponent implements OnInit {
   }
 
   signIn(): void {
-    if(this.validateForm()) {
+    if (this.validateForm()) {
       this.isLoading = true;
-      this.loginService.signIn(this.form.controls.login.value, this.form.controls.password.value).subscribe(
-        () => {
-        this.notificationService.success('Pomyślnie zalogowano uzytkownika', '');
-        this.modalService.close();
-        this.isLoading = false;
-      },
-      (error) => {
-        const errorMessage = JSON.parse(error.error);
-        this.form.controls['login'].setErrors({ invalidLogin: errorMessage.message });
-        this.notificationService.error('Logowanie', errorMessage.message);
-        this.isLoading = false;
-      });
+      this.loginService
+        .signIn(this.form.controls.login.value, this.form.controls.password.value)
+        .pipe(finalize(() => (this.isLoading = false)))
+        .subscribe(
+          () => {
+            this.notificationService.success('Pomyślnie zalogowano uzytkownika', '');
+            this.modalService.close();
+          },
+          (error) => {
+            const errorMessage = JSON.parse(error?.error);
+            this.form.controls['login'].setErrors({ invalidLogin: errorMessage.message });
+            this.notificationService.error('Logowanie', errorMessage.message);
+          }
+        );
     }
   }
 
   private validateForm(): boolean {
     if (!this.form.valid) {
-      Object.values(this.form.controls).forEach(control => {
+      Object.values(this.form.controls).forEach((control) => {
         if (control.invalid) {
           control.markAsDirty();
           control.updateValueAndValidity({ onlySelf: true });
