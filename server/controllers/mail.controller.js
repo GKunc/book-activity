27818717fdd31a91c1cdbdcf002868db392742ef
GKunc <grzeg.kunc@gmail.com>
2023-05-11 21:49:16 +0200

    Add proper user email

--- server/controllers/mail.controller.js
+++ server/controllers/mail.controller.js
@@ -1,12 +1,11 @@
 const nodemailer = require('nodemailer');
 const templates = require('../assets/email-template.js');
+const db = require('../models');
+const User = db.user;
 
 exports.sendConfirmationEmail = async (req) => {
   const userId = req.userId;
   const confirmationSecret = req.confirmationSecret;
-
-  console.log(process.env.EMAIL_USER);
-  console.log(process.env.EMAIL_PASSWORD);
 
   let transporter = nodemailer.createTransport({
     host: 'smtp.office365.com',
@@ -21,10 +20,14 @@ exports.sendConfirmationEmail = async (req) => {
   const message = 'Potwierdz adres email';
   const redirectURL = process.env.CONFRIMATION_URL + '?confirmationSecret=' + confirmationSecret + '&userId=' + userId;
 
+  const user = await User.findOne({
+    _id: userId,
+  });
+
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: 'book.activity@outlook.com', // sender address
-    to: 'gregson0307@gmail.com', // list of receivers
+    to: user.email, // list of receivers
     subject: 'Book Activity - Potwierdzenie rejestracji', // Subject line
     text: message,
     html: templates.HTML_TEMPLATE(redirectURL),
